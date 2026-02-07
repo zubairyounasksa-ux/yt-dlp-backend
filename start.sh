a8f4d65d661e6861342de6e10dff6dccbf0303cc
zubairyounasksa-ux <zubairyounasksa@gmail.com>
2026-02-07 21:13:35 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Make script executable
+chmod +x start.sh
+
 # Install yt-dlp
+echo "Installing yt-dlp..."
 pip install yt-dlp
 
-# Start the Node server
-npm install
-npm start
+# Start Node.js server
+echo "Starting server..."
+node index.js
